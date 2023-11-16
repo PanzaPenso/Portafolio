bef369cb70610660551639e38745e7b869ffcbac
EdPanza <57097021+PanzaPenso@users.noreply.github.com>
2023-11-16 19:29:15 +0100

    js conditionals exercises

---
+++ 03_Web_Development/10_js_conditionals/02_js_language_support/script.js
@@ -0,0 +1,13 @@
+let message = "";
+let language = "Chinese";
+
+if (language === "English"){
+  message = "Thank you";
+}else if (language === "Spanish"){
+  message = "Gracias";
+}else if (language === "German"){
+  message = "Danke";
+}else {
+  message = "We don't support " + language;
+}
+console.log(message);
