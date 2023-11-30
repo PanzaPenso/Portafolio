1245ce74e348c20dfed0caf9f09a94c0fa8d2f89
EdPanza <57097021+PanzaPenso@users.noreply.github.com>
2023-11-30 21:33:40 +0100

    exercise intermediate html and css

---
+++ 03_Web_Development/13_js_interactive_html/01_exercise/script.js
@@ -0,0 +1,12 @@
+let inputValue = 10;
+let milesToKm = true;
+let result = 0;
+if (milesToKm) {
+  result = inputValue * 1.60934;
+} else {
+  result = inputValue / 1.60934;
+}
+const resultString = inputValue + " miles are " + result + " km";
+console.log(resultString);
+const resultElement = document.getElementById('resultElement');
+resultElement.innerHTML = resultString;
